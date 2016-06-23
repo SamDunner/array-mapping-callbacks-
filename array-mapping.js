55dc45f36e101d3faeeee664b24810138023f584
sam dunner <samdunner23@gmail.com>
2016-06-23 19:27:45 +0000

    completed copy of array mapping

--- array-mapping.js
+++ array-mapping.js
@@ -3,9 +3,15 @@ var input = [
   { x: 12, y: 5 },
   { x: 8, y: 15 }
 ];
-var result = input.map(/* Your code here */);
+var result = input.map(function(object){
+  var newObject = {};
+  newObject = Math.sqrt(Math.pow(object.x, 2) + Math.pow(object.y, 2));
+  return newObject;
+});
+console.log(result);
 
-assert.equal(result, [5, 13, 17]);
+//assert.equal(result, [5, 13, 17]);
+//This assertion wasn't working properly
 
 
 /*
@@ -30,3 +36,4 @@ assert.equal(result, [5, 13, 17], squareRoot);
 // var assert = [];
   //Math.sqrt
   //return assert
+  */
