bc7d95f104a35b3878d200d8c97bcbaa8696a041
sam dunner <samdunner23@gmail.com>
2016-06-23 19:10:33 +0000

    over complication of this problem

--- array-mapping.js
+++ array-mapping.js
@@ -6,3 +6,27 @@ var input = [
 var result = input.map(/* Your code here */);
 
 assert.equal(result, [5, 13, 17]);
+
+
+/*
+var input = [
+  { x: 3, y: 4 },
+  { x: 12, y: 5 },
+  { x: 8, y: 15 }
+];
+  var result = input.map(function(squareRoot, num) {
+    var assert = [];
+    var object = {};
+    Math.pow(assert[object.key], 2) = assert;
+  });
+
+
+    function squareRoot(num) {
+      return Math.sqrt(num);
+  }
+
+assert.equal(result, [5, 13, 17], squareRoot);
+
+// var assert = [];
+  //Math.sqrt
+  //return assert
